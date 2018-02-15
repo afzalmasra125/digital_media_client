7bb1cabc0513c24944055e07c95db35113a9b164
afzalmasra125 <afzalmasra@gmail.com>
2018-02-14 21:27:35 -0600

    Day 1 changes

--- runner.rb
+++ runner.rb
@@ -2,6 +2,8 @@ require 'unirest'
 
 system 'clear'
 
+
+
     puts "Enter information for a new movie"
     client_params = {}
 
@@ -37,7 +39,7 @@ system 'clear'
     # else
     #   errors = response.body["errors"]
     #   puts
-    #   puts "Your contact did not save"
+    #   puts "Your movie did not save"
     #   puts "please look at the following reasons"
     #   puts "------------------------------------"
     #   errors.each do |error|
@@ -45,3 +47,52 @@ system 'clear'
     #   end
   #   end
   # end
+
+  print "Enter a movie id: "
+    input_id = gets.chomp
+
+    response = Unirest.get("http://localhost:3000/movies/#{input_id}")
+    movie = response.body
+
+    puts "Enter new information for movie ##{input_id}"
+    client_params = {}
+
+    print "First Name (#{movie["first_name"]}): "
+    client_params[:first_name] = gets.chomp
+
+    print "Middle Name (#{movie["middle_name"]}): "
+    client_params[:middle_name] = gets.chomp
+
+    print "Last Name (#{movie["last_name"]}): "
+    client_params[:last_name] = gets.chomp
+
+    print "Email (#{movie["email"]}): "
+    client_params[:email] = gets.chomp
+
+    print "Bio (#{movie["bio"]}): "
+    client_params[:bio] = gets.chomp
+
+    print "Phone Number (#{movie["phone_number"]}): "
+    client_params[:phone_number] = gets.chomp
+
+    client_params.delete_if {|key, value| value.empty? }
+
+    response = Unirest.patch(
+                            "http://localhost:3000/movies/#{input_id}",
+                            parameters: client_params
+                            )
+
+    if response.code == 200
+      movie = response.body
+      puts JSON.pretty_generate(movie)
+    else
+      errors = response.body["errors"]
+      puts
+      puts "Your movie did not update"
+      puts "please look at the following reasons"
+      puts "------------------------------------"
+      errors.each do |error|
+        puts error
+      end
+    end
+  end
